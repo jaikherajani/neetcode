2e9244c2093b2143720f360b81688bbdcb980f93
Jai Kumar Kherajani <jai.kherajani19@gmail.com>
2024-05-06 22:47:18 +0530

    347.TopKFrequentElements

--- src/main/java/org/example/TopKFrequentElements.java
+++ src/main/java/org/example/TopKFrequentElements.java
@@ -1,17 +1,33 @@
 package org.example;
 
 import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
 
-public class TopKFrequentElements{
+public class TopKFrequentElements {
 
     public int[] topKFrequent(int[] nums, int k) {
-
+        int[] res = new int[k];
         HashMap<Integer, Integer> fMap = new HashMap<>();
 
         for (int n : nums) {
             fMap.put(n, fMap.getOrDefault(n, 0) + 1);
         }
 
+        Set<Integer> fSet = fMap.keySet();
+        Queue<Integer> fQueue = new PriorityQueue<>((a, b) -> fMap.get(a) - fMap.get(b));
+        for (int s : fSet) {
+            fQueue.add(s);
+            if (fQueue.size() > k) {
+                fQueue.remove();
+            }
+        }
 
+        int i = 0;
+        while (!fQueue.isEmpty()) {
+            res[i++] = fQueue.remove();
+        }
+        return res;
     }
 }
